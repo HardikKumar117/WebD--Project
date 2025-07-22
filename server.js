1242d542e5d04d0a28b0970db4ad3c8cc8bf712b
Hardik Kumar <Hardikkumar885@gmail.com>
2025-07-22 21:15:52 +0530

    connected to mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { logger } from './src/logger.js'
 import {app} from './src/app.js'
+import { createDbConnection } from './src/db/connection.js'
 dotenv.config()
 
 
@@ -9,3 +10,4 @@ dotenv.config()
 app.listen(process.env.PORT,()=>{
 logger.info(`Server is running on port ${process.env.PORT}`)
 })
+createDbConnection()
